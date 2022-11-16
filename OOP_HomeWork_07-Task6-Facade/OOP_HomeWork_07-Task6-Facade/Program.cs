b7b7afa97005e506a53c34158ae9ca70666fa400
PanchenkoAl <45338702+PanchenkoAl@users.noreply.github.com>
2022-11-16 16:49:50 +0200

    Add files via upload

---
+++ OOP_HomeWork_07-Task6-Facade/OOP_HomeWork_07-Task6-Facade/Program.cs
@@ -0,0 +1,65 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Facade facade = new Facade();
+        facade.Show1();
+        facade.Show2();
+        Console.ReadKey();
+    }
+}
+public class HiddenOne
+{
+    public void FOne()
+    {
+        Console.WriteLine(" Hidden1");
+    }
+}
+public class HiddenTwo
+{
+    public void FTwo()
+    {
+        Console.WriteLine(" Hidden2");
+    }
+}
+public class HiddenThree
+{
+    public void FThree()
+    {
+        Console.WriteLine(" Hidden3");
+    }
+}
+public class HiddenFour
+{
+    public void FFour()
+    {
+        Console.WriteLine(" Hidden4");
+    }
+}
+public class Facade
+{
+    HiddenOne one;
+    HiddenTwo two;
+    HiddenThree three;
+    HiddenFour four;
+    public Facade()
+    {
+        one = new HiddenOne();
+        two = new HiddenTwo();
+        three = new HiddenThree();
+        four = new HiddenFour();
+    }
+    public void Show1()
+    {
+        Console.WriteLine("Show1 ---- ");
+        one.FOne();
+        two.FTwo();
+        four.FFour();
+    }
+    public void Show2()
+    {
+        Console.WriteLine("Show2 ---- ");
+        two.FTwo();
+        three.FThree();
+    }
+}
