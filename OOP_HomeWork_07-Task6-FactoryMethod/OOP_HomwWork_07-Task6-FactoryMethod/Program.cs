401a39617f6fd86475a11551e663fed7a7636392
PanchenkoAl <45338702+PanchenkoAl@users.noreply.github.com>
2022-11-16 17:28:26 +0200

    Add files via upload

---
+++ OOP_HomeWork_07-Task6-FactoryMethod/OOP_HomwWork_07-Task6-FactoryMethod/Program.cs
@@ -0,0 +1,50 @@
+﻿class MainApp
+{
+
+    static void Main()
+    {
+        ICreator[] creators = new ICreator[2];
+        creators[0] = new ConcreteCreator1();
+        creators[1] = new ConcreteCreator2();
+        
+        foreach (ICreator creator in creators)
+        {
+            IProduct product = creator.FactoryMethod();
+            Console.WriteLine("Created {0}", product.GetType().Name);
+        }
+        Console.ReadKey();
+    }
+}
+
+interface IProduct
+{
+}
+
+class ConcreteProduct1 : IProduct
+{
+}
+
+class ConcreteProduct2 : IProduct
+{
+}
+
+interface ICreator
+{
+    public IProduct FactoryMethod();
+}
+
+class ConcreteCreator1 : ICreator
+{
+    public IProduct FactoryMethod()
+    {
+        return new ConcreteProduct1();
+    }
+}
+
+class ConcreteCreator2: ICreator
+{
+    public IProduct FactoryMethod()
+    {
+        return new ConcreteProduct2();
+    }
+}
